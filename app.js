671a17e05347652e42491870f4f7bfbac01213a3
Ivan Antonuccio <ivan.leonnel@gmail.com>
2023-10-25 11:06:32 +0200

    Ocult el puerto y el host en variables de entorno

--- app.js
+++ app.js
@@ -1,41 +1,57 @@
-// Importa express
+// 1 - Importo express
 const express = require('express');
-// Importa las rutas
+// 18 - Importo las rutas ./routes/authRoutes
 const authRoutes = require('./routes/authRoutes');
-// Importa el middleware para cookies
+// 37 - Instalamos e importamos cookie-parser, un middleware para manejar y analizar las cookies
 const cookieParser = require('cookie-parser');
-// Crea una instancia de Express
+// 2 - Hago disponible express con la constante app
 const app = express();
+// ----------------------------------  ESPACIO RESERVADO PARA TRAER PAQUETES Y CONEXIONES PRINCIPALES----------------------------------- //
 
+// 3 - Importo dotenv qué le da seguridad a mis conexiones. Nos deja asignarle variables a cosas que quiero ocultar
 require('dotenv').config()
 
-// Habilita los middleware de la carpeta public
+
+// ---------------------*************  ESPACIO RESERVADO PARA TRAER LA CONEXIÓN A LA DB  *************--------------------------------- //
+
+// 13 - Habilito los middleware de la carpeta public
 app.use(express.static('public'));
-// Utiliza express para pasar JSON
+// 21 - Utilizo express para pasar los json que recibe Node (ej: los usuarios), a un objeto para que JavaScript pueda leerlos
 app.use(express.json());
-// Habilita el middleware cookie-parser
+// 38 - Habilito el middleware cookie-parser
 app.use(cookieParser());
-// Setea el motor de vistas EJS
+// 11 - Seteo el ejs
 app.set('view engine', 'ejs');
 
-// Ruta de inicio
+// 12 - Llamo a mi página de inicio. En este caso, será signup.ejs
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-// Agrega las rutas de autenticación
+// 19 - Llamamos a authRoutes y lo hacemos aquí, porque recién la vamos a necesitar después de home
 app.use(authRoutes);
 
+// -------------------//////////////// ESPACIO RESERVADO PARA LAS RUTAS ////////////////---------------------------------------------- //
+
+                                                //------------------------//
+
+// ---------------------*************  ESPACIO RESERVADO PARA TRAER LA CONEXIÓN A LA DB  *************------------------------------- //
+
+
+// 5 - Acá llamo al PUERTO localhost que lo tendré en el archivo .env
 // Obtiene el puerto de la variable de entorno PORT o usa el puerto 3000 si no está definido
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 // Escucha en el host 0.0.0.0 y el puerto proporcionado por Railway
-app.listen(port, "0.0.0.0", function () {
+app.listen(port, host, function () {
     console.log('Servidor ejecutándose');
 });
 
-// Configura la conexión a la base de datos
+// 4 - Traigo la conexión desde el archivo que tendrá la conexión a la Base de datos = db/conexion.js
 const connectDB = require('./db/conexion');
+
+// 6 - Creo una función para llamar a la conexión con la DB. Está conexión se encontrará en el archivo db/conexion.js esta conexión retornará a la función iniciar y se comunicará con el archivo .env en dónde se encuentra el link hacia la DB
 const iniciar = async () => {
     try {
         await connectDB(process.env.MONGO_URL);
@@ -44,5 +60,5 @@ const iniciar = async () => {
     }
 }
 
-// Llama a la función de inicio de la base de datos
+// 7 - Llamo a la función iniciar
 iniciar();
